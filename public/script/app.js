dca60fa5cc3d61535d21fd71867badfd41e82ff4
Kevin Wilusz <kwilusz10@gmail.com>
2021-10-14 15:06:37 +0200

    started on loginpage

--- public/script/app.js
+++ public/script/app.js
@@ -1,3 +1,28 @@
 'use strict';
-
 console.log('Iam alive')
+
+
+onload() = () => {
+    const form = document.createElement('form');
+    form.id = 'formen';
+    
+    const h1 = document.createElement('h1');
+    h1.textContent = "Login";
+    document.getElementById('formen').appendChild(h1);
+
+
+
+    const txt1 = document.createElement('textbox');
+    txt1.inputMode;
+    document.getElementById('formen').appendChild(txt1);
+
+    const txt2 = document.createElement('textbox');
+    txt2.inputMode;
+    document.getElementById('formen').appendChild(txt2);
+
+
+    document.getElementById('login').appendChild('formen');
+
+
+    
+}
